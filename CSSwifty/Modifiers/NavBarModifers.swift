3029f4980ae882e22ab36f415b44a1d48f4d705c
Michael Osuji <michaelchenko@gmail.com>
2021-08-19 02:33:58 -0400

    deleted a line

--- CSSwifty/Modifiers/NavBarModifers.swift
+++ CSSwifty/Modifiers/NavBarModifers.swift
@@ -28,7 +28,6 @@ struct NavigationBarModifier: ViewModifier {
         UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
     }
     
-    
     func body(content: Content) -> some View {
         ZStack{
             content
